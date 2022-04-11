11241f0cb785682f3540f3abdf5e57b78a75e484
Carlos <carlosheredia@multimediatarragona.com>
2022-04-11 16:43:49 +0200

    commit 2

--- eclipse-workspace/PruebasGit/src/Pruebas.java
+++ eclipse-workspace/PruebasGit/src/Pruebas.java
@@ -4,6 +4,8 @@ public class Pruebas {
 	public static void main(String[] argumentos) {
 		
 		System.out.println("utilizando git");
+		System.out.println("utilizando git 2");
+		System.out.println("utilizando git 3");
 	
 	}
 
